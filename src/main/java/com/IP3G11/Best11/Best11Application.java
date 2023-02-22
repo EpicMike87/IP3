208f5ccc8e2958afd7b64d9b2ab99fd4cb024561
seanlaughlin2 <seanslaughlin@gmail.com>
2023-02-22 19:44:34 +0000

    Fix on file issue

---
+++ src/main/java/com/IP3G11/Best11/Best11Application.java
@@ -0,0 +1,17 @@
+package com.IP3G11.Best11;
+
+import com.IP3G11.Best11.repositories.PlayerApiRepo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+import java.io.IOException;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class Best11Application {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        SpringApplication.run(Best11Application.class, args);
+    }
+
+}
