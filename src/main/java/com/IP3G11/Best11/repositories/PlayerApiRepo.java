7c1b214ac0505d63779764b15d97bc5eef2d0e56
seanlaughlin2 <seanslaughlin@gmail.com>
2023-02-22 23:04:36 +0000

    Tweaks

--- src/main/java/com/IP3G11/Best11/repositories/PlayerApiRepo.java
+++ src/main/java/com/IP3G11/Best11/repositories/PlayerApiRepo.java
@@ -18,10 +18,13 @@ public class PlayerApiRepo {
 
     //SPL League ID
     private static final int leagueId = 179;
+    private final List<Team> teams = getTeams();
+
+    public PlayerApiRepo() throws IOException, InterruptedException {
+    }
 
     //Get ID for team from team name supplied
-    public static int getTeamId(String teamName) throws IOException, InterruptedException {
-        List<Team> teams = getTeams();
+    public int getTeamId(String teamName) throws IOException, InterruptedException {
         for(Team t : teams){
             if(t.getTeamName().equalsIgnoreCase(teamName)) {
                 return t.getId();
@@ -30,7 +33,7 @@ public class PlayerApiRepo {
         return 0;
     }
 
-    public static List<Team> getTeams() throws IOException, InterruptedException {
+    private List<Team> getTeams() throws IOException, InterruptedException {
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://api-football-v1.p.rapidapi.com/v3/teams?league=" + leagueId + "&season=2022"))
                 .header("X-RapidAPI-Key", "9e3324bf83msh34dc07c79189889p1f8c13jsn975dfb9aa4c5")
                 .header("X-RapidAPI-Host", "api-football-v1.p.rapidapi.com")
--- src/main/java/com/IP3G11/Best11/services/PlayerService.java
+++ src/main/java/com/IP3G11/Best11/services/PlayerService.java
@@ -1,4 +1,31 @@
 package com.IP3G11.Best11.services;
 
+import com.IP3G11.Best11.model.Player;
+import com.IP3G11.Best11.model.Team;
+import com.IP3G11.Best11.repositories.PlayerApiRepo;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
 public class PlayerService {
+
+    private PlayerApiRepo repo;
+
+    public PlayerService() throws IOException, InterruptedException {
+        repo = new PlayerApiRepo();
+    }
+
+    public int getTeamId(String teamName) throws IOException, InterruptedException {
+        return repo.getTeamId(teamName);
+    }
+
+    public Team getTeamByName(String teamName){
+        return null;
+    }
+
+    public Player getPlayerByName(String name){
+        return null;
+    }
+
 }
