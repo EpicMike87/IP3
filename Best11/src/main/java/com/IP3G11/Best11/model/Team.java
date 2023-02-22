7382adc18036da2df493a108a61cc722ef46e6b9
seanlaughlin <71667909+seanlaughlin@users.noreply.github.com>
2023-02-22 18:00:45 +0000

    Added Team class attributes

--- Best11/src/main/java/com/IP3G11/Best11/model/Team.java
+++ Best11/src/main/java/com/IP3G11/Best11/model/Team.java
@@ -1,4 +1,17 @@
 package com.IP3G11.Best11.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Team {
+
+    private String name;
+    private List<Player> players;
+
 }
