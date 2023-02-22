94191b52f4d6f1af22529a929a4da58d16738a19
seanlaughlin2 <seanslaughlin@gmail.com>
2023-02-22 23:04:53 +0000

    Tweaks

--- src/main/java/com/IP3G11/Best11/Best11Application.java
+++ src/main/java/com/IP3G11/Best11/Best11Application.java
@@ -1,5 +1,6 @@
 package com.IP3G11.Best11;
 
+import com.IP3G11.Best11.services.PlayerService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -11,6 +12,8 @@ public class Best11Application {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         SpringApplication.run(Best11Application.class, args);
+        PlayerService service = new PlayerService();
+        System.out.println(service.getTeamId("hearts"));
     }
 
 }
