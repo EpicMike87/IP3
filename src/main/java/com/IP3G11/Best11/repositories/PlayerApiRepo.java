746d233016e81f3540479955b974fb1cc48897d9
seanlaughlin2 <seanslaughlin@gmail.com>
2023-02-23 14:30:10 +0000

    Added method to determine subclass of player and populate fields

--- src/main/java/com/IP3G11/Best11/repositories/PlayerApiRepo.java
+++ src/main/java/com/IP3G11/Best11/repositories/PlayerApiRepo.java
@@ -1,7 +1,6 @@
 package com.IP3G11.Best11.repositories;
 
-import com.IP3G11.Best11.model.Player;
-import com.IP3G11.Best11.model.Team;
+import com.IP3G11.Best11.model.*;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
@@ -12,15 +11,11 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 
+// TODO: 23/02/2023 Populate team objects with players in getTeams
 public class PlayerApiRepo {
 
     //SPL League ID
@@ -33,8 +28,8 @@ public class PlayerApiRepo {
     //Get ID for team from team name supplied
     public int getTeamId(String teamName) throws IOException, InterruptedException {
         List<Team> teams = getTeams();
-        for(Team t : teams){
-            if(t.getTeamName().equalsIgnoreCase(teamName)) {
+        for (Team t : teams) {
+            if (t.getTeamName().equalsIgnoreCase(teamName)) {
                 return t.getId();
             }
         }
@@ -42,7 +37,7 @@ public class PlayerApiRepo {
     }
 
     public List<Team> getTeams() throws IOException, InterruptedException {
-        if(teams == null) {
+        if (teams == null) {
             HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://api-football-v1.p.rapidapi.com/v3/teams?league=" + leagueId + "&season=2022"))
                     .header("X-RapidAPI-Key", "9e3324bf83msh34dc07c79189889p1f8c13jsn975dfb9aa4c5")
                     .header("X-RapidAPI-Host", "api-football-v1.p.rapidapi.com")
@@ -72,14 +67,12 @@ public class PlayerApiRepo {
     }
 
     public Player getPlayerByName(String name) throws IOException, InterruptedException, NullPointerException {
-//        Get Player by Name
 
-//        playerName will have to replace spaces in between first and last with %20
-
-//        String playerName = "Maeda";
+        //playerName needs spaces replaced in between first and last name with %20 as required by API
         String playerName = name;
         playerName = playerName.replace(" ", "%20");
 
+        //Make API request and get response
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("https://api-football-v1.p.rapidapi.com/v3/players?league=" + leagueId + "&season=2022&search=" + playerName))
                 .header("X-RapidAPI-Key", "9e3324bf83msh34dc07c79189889p1f8c13jsn975dfb9aa4c5")
@@ -87,60 +80,109 @@ public class PlayerApiRepo {
                 .method("GET", HttpRequest.BodyPublishers.noBody())
                 .build();
         HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
+
         String jsonString = response.body();
         JsonElement json = JsonParser.parseString(jsonString);
         JsonObject jsonObject = json.getAsJsonObject();
         JsonArray playerInfo = jsonObject.getAsJsonArray("response");
-        Player player1 = new Player();
 
-        for(int i = 0; i < playerInfo.size(); i++){
-            JsonObject player = playerInfo.get(i).getAsJsonObject();
-//            System.out.println(player);
-            player1.setIdNo(Integer.parseInt(player.get("player").getAsJsonObject().get("id").toString()));
-            player1.setFirstName(player.get("player").getAsJsonObject().get("firstname").getAsString());
-            player1.setLastName(player.get("player").getAsJsonObject().get("lastname").getAsString());
-            player1.setAge(Integer.parseInt(player.get("player").getAsJsonObject().get("age").toString()));
-            String dob = player.get("player").getAsJsonObject().get("birth").getAsJsonObject().get("date").toString();
+        JsonObject player = playerInfo.get(0).getAsJsonObject();
 
-            // Used to remove "" from the string
-            String date = removeQMarks(dob);
-//            System.out.println(date);
-            LocalDate playerDob = LocalDate.parse(date);
-//            System.out.println(playerDob);
-            player1.setDateOfBirth(playerDob);
-            player1.setPhotoUrl(player.get("player").getAsJsonObject().get("photo").getAsString());
-            player1.setTeam(player.get("statistics").getAsJsonArray().get(0).getAsJsonObject().get("team").getAsJsonObject().get("name").getAsString());
-//            System.out.println(playerTeam);
-            player1.setPosition(player.get("statistics").getAsJsonArray().get(0).getAsJsonObject().get("games").getAsJsonObject().get("position").getAsString());
-//            System.out.println("This is position  " + playerPosition);
-            player1.setMatchesPlayed(Integer.parseInt(player.get("statistics").getAsJsonArray().get(0).getAsJsonObject().get("games").getAsJsonObject().get("appearences").toString()));
-//            System.out.println(player1.getMatchesPlayed());
-            player1.setCaptain(Boolean.parseBoolean(player.get("statistics").getAsJsonArray().get(0).getAsJsonObject().get("games").getAsJsonObject().get("captain").toString()));
-            double rate = player.get("statistics").getAsJsonArray().get(0).getAsJsonObject().get("games").getAsJsonObject().get("rating").getAsDouble();
-//            System.out.println(rate);
-            player1.setRating(rate);
-            player1.setFoulsCommitted(Integer.parseInt(player.get("statistics").getAsJsonArray().get(0).getAsJsonObject().get("fouls").getAsJsonObject().get("committed").toString()));
-            player1.setYellowCards(Integer.parseInt(player.get("statistics").getAsJsonArray().get(0).getAsJsonObject().get("cards").getAsJsonObject().get("yellow").toString()));
-            player1.setRedCards(Integer.parseInt(player.get("statistics").getAsJsonArray().get(0).getAsJsonObject().get("cards").getAsJsonObject().get("red").toString()));
-            player1.setPasses(Integer.parseInt(player.get("statistics").getAsJsonArray().get(0).getAsJsonObject().get("passes").getAsJsonObject().get("total").toString()));
-            player1.setPassAccuracy(player.get("statistics").getAsJsonArray().get(0).getAsJsonObject().get("passes").getAsJsonObject().get("accuracy").getAsDouble());
+        //Determines subclass and populates all fields from API data
+        Player player1 = populateFieldsOfPlayer(player);
 
-        }
-
-//        System.out.println(player1);
         return player1;
 
     }
 
     /**
      * Method removes "" for the date variable
+     *
      * @param s1
      * @return
      */
-    public static String removeQMarks(String s1){
+    public static String removeQMarks(String s1) {
         String s2 = s1.substring(1, s1.length() - 1);
         return s2;
+    }
+
+    private Player populateFieldsOfPlayer(JsonObject playerJson) {
+
+        //Get position from API data
+        String pos = playerJson.get("statistics").getAsJsonArray().get(0).getAsJsonObject().get("games").getAsJsonObject().get("position").getAsString();
+        Player player;
+
+        //Initialise player subclass
+        switch (pos) {
+            case "Attacker":
+                Attacker attk = new Attacker();
+                attk.setAssists(playerJson.get("statistics").getAsJsonArray().get(0).getAsJsonObject().get("goals").getAsJsonObject().get("assists").getAsInt());
+                attk.setGoals(playerJson.get("statistics").getAsJsonArray().get(0).getAsJsonObject().get("goals").getAsJsonObject().get("total").getAsInt());
+                attk.setShots(playerJson.get("statistics").getAsJsonArray().get(0).getAsJsonObject().get("shots").getAsJsonObject().get("total").getAsInt());
+                attk.setShotsOnTarget(playerJson.get("statistics").getAsJsonArray().get(0).getAsJsonObject().get("shots").getAsJsonObject().get("on").getAsInt());
+                attk.setPenaltiesScored(playerJson.get("statistics").getAsJsonArray().get(0).getAsJsonObject().get("penalty").getAsJsonObject().get("scored").getAsInt());
+                attk.setPenaltiesTaken(attk.getPenaltiesScored() + playerJson.get("statistics").getAsJsonArray().get(0).getAsJsonObject().get("penalty").getAsJsonObject().get("missed").getAsInt());
+                player = attk;
+                break;
+            case "Defender":
+                Defender dfnd = new Defender();
+                dfnd.setDuels(playerJson.get("statistics").getAsJsonArray().get(0).getAsJsonObject().get("duels").getAsJsonObject().get("total").getAsInt());
+                dfnd.setDuelsWon(playerJson.get("statistics").getAsJsonArray().get(0).getAsJsonObject().get("duels").getAsJsonObject().get("won").getAsInt());
+                dfnd.setTackles(playerJson.get("statistics").getAsJsonArray().get(0).getAsJsonObject().get("tackles").getAsJsonObject().get("total").getAsInt());
+                dfnd.setBlocks(playerJson.get("statistics").getAsJsonArray().get(0).getAsJsonObject().get("tackles").getAsJsonObject().get("blocks").getAsInt());
+                dfnd.setInterceptions(playerJson.get("statistics").getAsJsonArray().get(0).getAsJsonObject().get("tackles").getAsJsonObject().get("interceptions").getAsInt());
+                player = dfnd;
+                break;
+            case "Midfielder":
+                Midfielder mdfd = new Midfielder();
+                mdfd.setAssists(playerJson.get("statistics").getAsJsonArray().get(0).getAsJsonObject().get("goals").getAsJsonObject().get("assists").getAsInt());
+                mdfd.setGoals(playerJson.get("statistics").getAsJsonArray().get(0).getAsJsonObject().get("goals").getAsJsonObject().get("total").getAsInt());
+                mdfd.setTackles(playerJson.get("statistics").getAsJsonArray().get(0).getAsJsonObject().get("tackles").getAsJsonObject().get("total").getAsInt());
+                mdfd.setBlocks(playerJson.get("statistics").getAsJsonArray().get(0).getAsJsonObject().get("tackles").getAsJsonObject().get("blocks").getAsInt());
+                mdfd.setInterceptions(playerJson.get("statistics").getAsJsonArray().get(0).getAsJsonObject().get("tackles").getAsJsonObject().get("interceptions").getAsInt());
+                player = mdfd;
+                break;
+            case "Goalkeeper":
+                Goalkeeper gkpr = new Goalkeeper();
+                gkpr.setSaves(playerJson.get("statistics").getAsJsonArray().get(0).getAsJsonObject().get("goals").getAsJsonObject().get("saves").getAsInt());
+                gkpr.setGoalsConceded(playerJson.get("statistics").getAsJsonArray().get(0).getAsJsonObject().get("goals").getAsJsonObject().get("conceded").getAsInt());
+                player = gkpr;
+                break;
+            default:
+                player = new Player();
+        }
+
+        player.setIdNo(Integer.parseInt(playerJson.get("player").getAsJsonObject().get("id").toString()));
+        player.setFirstName(playerJson.get("player").getAsJsonObject().get("firstname").getAsString());
+        player.setLastName(playerJson.get("player").getAsJsonObject().get("lastname").getAsString());
+        player.setAge(Integer.parseInt(playerJson.get("player").getAsJsonObject().get("age").toString()));
+        String dob = playerJson.get("player").getAsJsonObject().get("birth").getAsJsonObject().get("date").toString();
+
+        String date = removeQMarks(dob);
+
+        LocalDate playerDob = LocalDate.parse(date);
+
+        player.setDateOfBirth(playerDob);
+        player.setPhotoUrl(playerJson.get("player").getAsJsonObject().get("photo").getAsString());
+        player.setTeam(playerJson.get("statistics").getAsJsonArray().get(0).getAsJsonObject().get("team").getAsJsonObject().get("name").getAsString());
+
+        player.setPosition(pos);
+
+        player.setMatchesPlayed(Integer.parseInt(playerJson.get("statistics").getAsJsonArray().get(0).getAsJsonObject().get("games").getAsJsonObject().get("appearences").toString()));
+
+        player.setCaptain(Boolean.parseBoolean(playerJson.get("statistics").getAsJsonArray().get(0).getAsJsonObject().get("games").getAsJsonObject().get("captain").toString()));
+        double rate = playerJson.get("statistics").getAsJsonArray().get(0).getAsJsonObject().get("games").getAsJsonObject().get("rating").getAsDouble();
+
+        player.setRating(rate);
+        player.setFoulsCommitted(playerJson.get("statistics").getAsJsonArray().get(0).getAsJsonObject().get("fouls")
+                .getAsJsonObject().get("committed").isJsonNull() ? 0 :
+                playerJson.get("statistics").getAsJsonArray().get(0).getAsJsonObject().get("fouls").getAsJsonObject().get("committed").getAsInt());
+        player.setYellowCards(Integer.parseInt(playerJson.get("statistics").getAsJsonArray().get(0).getAsJsonObject().get("cards").getAsJsonObject().get("yellow").toString()));
+        player.setRedCards(Integer.parseInt(playerJson.get("statistics").getAsJsonArray().get(0).getAsJsonObject().get("cards").getAsJsonObject().get("red").toString()));
+        player.setPasses(Integer.parseInt(playerJson.get("statistics").getAsJsonArray().get(0).getAsJsonObject().get("passes").getAsJsonObject().get("total").toString()));
+        player.setPassAccuracy(playerJson.get("statistics").getAsJsonArray().get(0).getAsJsonObject().get("passes").getAsJsonObject().get("accuracy").getAsDouble());
+
+        return player;
     }
 
 }
