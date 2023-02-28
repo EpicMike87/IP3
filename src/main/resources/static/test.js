997be678617766f7d6458cbb1e356b29a627dac9
seanlaughlin2 <seanslaughlin@gmail.com>
2023-02-28 21:00:20 +0000

    Front end data sorting test files

--- src/main/resources/static/test.js
+++ src/main/resources/static/test.js
@@ -1,8 +1,8 @@
 fetch('http://localhost:8080/team/celtic')
 .then(response => response.json()) 
-.then(html => {
-    console.log(html);
-    const players = html.players;
+.then(data => {
+    console.log(data);
+    const players = data.players;
     console.log(players)
     players.sort((a, b) => (a.matchesPlayed > b.matchesPlayed) ? -1 : 1)
     players.forEach(player => {
