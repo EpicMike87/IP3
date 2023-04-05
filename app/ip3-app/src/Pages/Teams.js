31c724f7abbf9f2e80d04dc8b6247414108dd39b
EpicMike87 <mander234@caledonian.ac.uk>
2023-04-05 19:11:56 +0100

    Conflict
    Fixing errors after merge conflict

--- app/ip3-app/src/Pages/Teams.js
+++ app/ip3-app/src/Pages/Teams.js
@@ -81,26 +81,6 @@ function Teams() {
         }
     };
 
-    const responsive = {
-        superLargeDesktop: {
-            // the naming can be any, depends on you.
-            breakpoint: { max: 4000, min: 3000 },
-            items: 5
-        },
-        desktop: {
-            breakpoint: { max: 3000, min: 1024 },
-            items: 3
-        },
-        tablet: {
-            breakpoint: { max: 1024, min: 464 },
-            items: 2
-        },
-        mobile: {
-            breakpoint: { max: 464, min: 0 },
-            items: 1
-        }
-    };
-
     useEffect(() => {
         const teamId = searchParams.get("id");
         const teamName = searchParams.get("name");
@@ -136,24 +116,12 @@ function Teams() {
             });
     }
 
-    const searchTeamByName = (name) => {
-        Api.get(`/team/${name}`)
-            .then(res => {
-                mapData(res.data);
-            }).then(showPlayerSection())
-            .catch(err => {
-                console.log(err);
-            });
-    }
-
-
     const searchTeam = () => {
         if (team.length > 2) {
             Api.get(`/team/${team}`)
                 .then(res => {
                     mapData(res.data);
                     showPlayerSection();
-                })
                 }).then(showPlayerSection())
                 .catch(err => {
                     console.log(err);
