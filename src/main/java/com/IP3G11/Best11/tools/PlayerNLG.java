9a96217972e8a708f81d16e70aef8cc9b3555407
EpicMike87 <mander234@caledonian.ac.uk>
2023-04-20 18:08:35 +0100

    Adding PlayerNLG class
    Adding the PlayerNLG class to repo

---
+++ src/main/java/com/IP3G11/Best11/tools/PlayerNLG.java
@@ -0,0 +1,36 @@
+        package com.IP3G11.Best11.tools;
+
+        import com.IP3G11.Best11.model.Player;
+
+        import java.util.HashMap;
+        import java.util.List;
+
+public class PlayerNLG {
+
+    static String ordinal;
+    static String leaguePositionText;
+    static String additionalPositionText;
+    HashMap<String, Double> averages;
+    double goalsAvg;
+    double concededAvg;
+
+    public PlayerNLG(List<Player> player) {
+        goalsAvg = averages.get("Total Average");
+        concededAvg = averages.get("Total Average");
+    }
+
+    public String playerText(Player player) {
+      /*
+      The following is the logic to use the correct ordinal (2nd, 3rd, 4th etc) when referring to the team's position.
+      It will also use the terms "top of the league" and "bottom of the league" to refer to the teams in 1st and 12th.
+      */
+
+        String playerGoals = String.format("%s has scored 20 in %1s appearances this season", player.getLastName(), player.getMatchesPlayed());
+        String playerRating = String.format("they have an average rating of %1s", player.getRating());
+
+
+        String playerNLGString = playerGoals + playerRating;
+
+        return playerNLGString;
+    }
+}
